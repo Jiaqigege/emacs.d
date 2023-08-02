9edc4277392a4d629242223ae4a66c9ce97927c5
Jiaqigege <lixsh1130@163.com>
2023-08-02 18:32:23 +0800

    feat: add some basic config.

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+
 ;; close tool bar
 (tool-bar-mode -1)
 ;; close scroll bar
@@ -5,5 +6,16 @@
 
 ;; close startup tutorial page
 (setq inhibit-startup-screen t)
+
+;; set default direcotry to the home directory
+(setq default-directory "~/")
+
+;; ask whether they really want to quit to prevent accidental closures
+(setq confirm-kill-emacs #'yes-or-no-p)
+
+;; 
+(add-hook 'prog-mode-hook #'show-paren-mode)
+
+(column-number-mode t)
 
 
