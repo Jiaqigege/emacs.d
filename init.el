33478f673edc949cd23e85c66c83b79fce0c2f9c
Jiaqigege <lixsh1130@163.com>
2023-08-04 10:19:39 +0800

    feat:.

--- init.el
+++ init.el
@@ -1,9 +1,13 @@
 (add-to-list 'load-path
 	     (expand-file-name (concat user-emacs-directory "lisp")))
 
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
 (require 'init-startup)
 (require 'init-elpa)
 (require 'init-ui)
 
 (use-package restart-emacs)
 
+(when (file-exists-p custom-file)
+	(load-file custom-file))
